36fdc0d7244f44c39ba0a472dacb2b7925a18e7b
Julian Barrios <julianbarrios0821@gmail.com>
2022-02-03 15:32:24 -0600

    rowEmpty component

--- src/components/wordle.tsx
+++ src/components/wordle.tsx
@@ -1,8 +1,12 @@
 import RowCompleted from "./rowCompleted";
+import RowEmpty from "./rowEmpty";
 
 export default function Wordle() {
     return <div>
         <RowCompleted word='sabio' solution="brake" />
-
+        <RowEmpty />
+        <RowEmpty />
+        <RowEmpty />
+        <RowEmpty />
     </div>
 }
